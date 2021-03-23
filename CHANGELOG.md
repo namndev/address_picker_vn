21d298c17a68c50ef0ef91f2dc38a463a27790f4
Nam Nguyen <nam.nd@teko.vn>
2021-03-23 11:12:49 +0700

    publish

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Address picker (Vietnam only)
+## v 0.3.0
+-   Init package
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/namnd/.develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/namnd/Documents/address_picker"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=0.2.0"
+export "FLUTTER_BUILD_NUMBER=0.2.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
